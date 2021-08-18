9b8c0928141e17afeaa54a509727b8be7b94e134
Prashant Srivastava <prashantsrivastava381@gmail.com>
2021-08-18 23:07:02 +0530

    added all file again

--- src/mycomponent/Navbar.js
+++ src/mycomponent/Navbar.js
@@ -1,29 +1,22 @@
-import React from 'react'
+import React from "react";
 
 export default function Navbar() {
-    return (
-        <>
-        <nav className="navbar navbar-expand-lg ">
-       <div className="container-fluid">
-         <a className="navbar-brand " href="/"><img className="sayF" src={"./brandLogo-triangle.png"} alt="" /></a>
-       
-         
-           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            
-            
-           </ul>
-           <form className="d-flex">
-         
-           <button type="button "><span></span>Get Early Access</button>
-           </form>
-         
-       </div>
-     </nav>
-     
-     
-     
-     
-     
-        </>
-    )
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg ">
+        <div className="container-fluid">
+          <a className="navbar-brand " href="/">
+            <img className="sayF" src={"./brandLogo-triangle.png"} alt="" />
+          </a>
+
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0"></ul>
+          <form className="d-flex">
+            <button type="button " className="buttonTop">
+              Get Early Access
+            </button>
+          </form>
+        </div>
+      </nav>
+    </>
+  );
 }
